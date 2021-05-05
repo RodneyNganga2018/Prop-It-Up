f9466f16b40e6903c6535e360b4fa48e79c35561
Rodney Nganga <rnganga2018@gmail.com>
2021-05-04 19:02:49 -0500

    incrementing age implemented

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    birthday = () => {
+        this.setState({ age: this.state.age + 1 });
+    }
+
     render () {
-        const { firstName, lastName, age, hairColor} = this.props;
+        const { firstName, lastName, hairColor, age} = this.props;
         return (
         <div>
+            <p>{ console.log(this.state.age) }</p>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {this.state.age}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={this.birthday}>Birthday Button For {firstName} {lastName}</button>
         </div>
         )
     }
